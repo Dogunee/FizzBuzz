993031022009095a38450368927af17c5021607e
Evan <evan@Evans-MacBook-Pro.local>
2024-09-09 17:45:32 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 0;
+        doFizzBuzz(i);
+    }
+
+    private static void doFizzBuzz(int i) {
+        while (i < 101) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -27,8 +32,8 @@ class FizzBuzz {
             } else {
 
                 System.out.println(i);
-
             }
+            i++;
         }
     }
 }
